043f006ab523932d20508aa6dd3702a3ede0ae27
Gabriel Venancio de Avelar <99gabrielavelar@gmail.com>
2024-06-23 21:58:52 -0300

    feat: criação da função para obter e retornar modelo armazenado no s3 na api

--- src/api/service/model_service.py
+++ src/api/service/model_service.py
@@ -6,3 +6,20 @@ import os
 session = boto3.Session()
 # Criar um cliente S3 a partir da sessão boto3
 s3_client = session.client('s3')
+
+def get_model(): 
+    # Nome do bucket S3 onde o modelo está armazenado
+    bucket_name = os.getenv("BUCKET_NAME")
+    # Chave (caminho) do arquivo do modelo no bucket S3
+    key = 'caminho para o seu modelo.joblib no S3'
+    try:
+        # Obter o objeto do S3
+        response = s3_client.get_object(Bucket=bucket_name, Key=key)
+        # Ler os bytes do corpo da resposta
+        model_bytes = response['Body'].read()
+        # Carregar o modelo a partir dos bytes usando joblib
+        return joblib.load(BytesIO(model_bytes))
+    
+    except Exception as e:
+        # Lançar uma exceção em caso de erro ao carregar o modelo do S3
+        raise Exception("Erro ao carregar modelo do S3") from e
