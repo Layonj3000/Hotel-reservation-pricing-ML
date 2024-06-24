f3ed0308db14290e6fbfcd11af1b4b10536d481a
Gabriel Venancio de Avelar <99gabrielavelar@gmail.com>
2024-06-23 21:36:11 -0300

    feat: adição da rota de predição e página inicial na api

--- src/api/main.py
+++ src/api/main.py
@@ -5,6 +5,12 @@ from routes.home_route import router as home_router
 # Criar uma instância do aplicativo FastAPI
 app = FastAPI()
 
+# Incluir o roteador de predições
+app.include_router(prediction_router)
+
+# Incluir o roteador da página inicial
+app.include_router(home_router)
+
 if __name__ == '__main__':
     import uvicorn
     # Executar o servidor Uvicorn com o aplicativo FastAPI
