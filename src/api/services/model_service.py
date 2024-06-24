800bb8d40c190a3852bf26de5bebd57fde18b16b
Gabriel Venancio de Avelar <99gabrielavelar@gmail.com>
2024-06-24 11:25:44 -0300

    fix: correção de configurações para docker compose na api

--- src/api/services/model_service.py
+++ src/api/services/model_service.py
@@ -3,15 +3,19 @@ import joblib
 from io import BytesIO
 import os
 # Criar uma sessão boto3
-session = boto3.Session()
+session = boto3.Session(
+    aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY"),
+    aws_session_token = os.getenv("AWS_SESSION_TOKEN"),
+)
 # Criar um cliente S3 a partir da sessão boto3
 s3_client = session.client('s3')
 
 def get_model(): 
     # Nome do bucket S3 onde o modelo está armazenado
-    bucket_name = os.getenv("BUCKET_NAME")
-    # Chave (caminho) do arquivo do modelo no bucket S3
-    key = 'caminho para o seu modelo.joblib no S3'
+    bucket_name = 'Nome do Bucket S3'
+    # Chave (caminho) do arquivo do modelo no Bucket S3
+    key = 'Caminho para o modelo.joblib no Bucket S3'
     try:
         # Obter o objeto do S3
         response = s3_client.get_object(Bucket=bucket_name, Key=key)
